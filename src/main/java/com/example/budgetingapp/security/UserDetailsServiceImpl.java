6003ae13970c8262a2a7f62ab8a683b104c3c468
Adam Mudrak <mudrets2000@gmail.com>
2024-10-01 13:39:17 +0300

    Add security

---
+++ src/main/java/com/example/budgetingapp/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.budgetingapp.security;
+
+import com.example.budgetingapp.exceptions.EntityNotFoundException;
+import com.example.budgetingapp.repositories.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException(
+                "Can't find user by email " + email));
+    }
+}
