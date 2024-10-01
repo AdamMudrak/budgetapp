1e33d81acb7403d87a1afd8fcaa46de992740898
Adam Mudrak <mudrets2000@gmail.com>
2024-10-01 14:01:58 +0300

    Add user mapper

---
+++ src/main/java/com/example/budgetingapp/mappers/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.budgetingapp.mappers;
+
+import com.example.budgetingapp.config.MapperConfig;
+import com.example.budgetingapp.dtos.user.request.UserRegistrationRequestDto;
+import com.example.budgetingapp.dtos.user.response.UserRegistrationResponseDto;
+import com.example.budgetingapp.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+    User toUser(UserRegistrationRequestDto userRegistrationRequestDto);
+
+    UserRegistrationResponseDto toUserResponseDto(User user);
+}
