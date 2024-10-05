e0b032a995429ccebbb7c20a0d8a847aa1c0f709
Adam Mudrak <mudrets2000@gmail.com>
2024-10-06 00:59:30 +0300

    Add sec constants

---
+++ src/main/java/com/example/budgetingapp/security/SecurityConstants.java
@@ -0,0 +1,13 @@
+package com.example.budgetingapp.security;
+
+public class SecurityConstants {
+    public static final String ACCESS = "ACCESS";
+    public static final String RESET = "RESET";
+    public static final String REFRESH = "REFRESH";
+    public static final String RESET_PASSWORD_SUBJECT = "Reset password  for Moneta";
+    public static final String RESET_PASSWORD_BODY = """
+            Good day! This email is to help you
+            reset password for your moneta account.
+            Please, use this volatile token to reset password.
+            """;
+}
