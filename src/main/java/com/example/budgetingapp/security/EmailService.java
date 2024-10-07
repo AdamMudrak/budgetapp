6c274d472807be3da17cac92db77e200d694e48b
Adam Mudrak <mudrets2000@gmail.com>
2024-10-07 15:54:33 +0300

    Modify email service

--- src/main/java/com/example/budgetingapp/security/EmailService.java
+++ src/main/java/com/example/budgetingapp/security/EmailService.java
@@ -1,23 +1,29 @@
 package com.example.budgetingapp.security;
 
-import static com.example.budgetingapp.security.SecurityConstants.RESET_PASSWORD_BODY;
-import static com.example.budgetingapp.security.SecurityConstants.RESET_PASSWORD_SUBJECT;
+import static com.example.budgetingapp.security.SecurityConstants.PATH;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Setter
 public class EmailService {
     private final JavaMailSender mailSender;
+    private final EmailSecretProvider emailSecretProvider;
+    private String subject;
+    private String body;
 
     public void sendSimpleEmail(String to, String token) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
-        message.setSubject(RESET_PASSWORD_SUBJECT);
-        message.setText(RESET_PASSWORD_BODY + System.lineSeparator() + token);
+        message.setSubject(subject);
+        message.setText(body + System.lineSeparator() + PATH
+                + emailSecretProvider.getEmailSecret()
+                + "=" + token);
         mailSender.send(message);
     }
 }
