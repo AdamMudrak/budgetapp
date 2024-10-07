352fa8c6705ea6b3671b21c11f62697ceca2ddbe
Adam Mudrak <mudrets2000@gmail.com>
2024-10-07 15:54:48 +0300

    Modify resetTokenRepo

--- src/main/java/com/example/budgetingapp/repositories/resetpassword/ResetTokenRepository.java
+++ src/main/java/com/example/budgetingapp/repositories/resetpassword/ResetTokenRepository.java
@@ -1,7 +1,9 @@
 package com.example.budgetingapp.repositories.resetpassword;
 
 import com.example.budgetingapp.entities.ResetToken;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResetTokenRepository extends JpaRepository<ResetToken, Long> {
+    Optional<ResetToken> findByResetToken(String resetToken);
 }
