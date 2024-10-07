a70202dbb7a094e90cd7701539470bfe1bb03504
Adam Mudrak <mudrets2000@gmail.com>
2024-10-07 15:55:36 +0300

    Add new endpoints

--- src/main/java/com/example/budgetingapp/controllers/AuthController.java
+++ src/main/java/com/example/budgetingapp/controllers/AuthController.java
@@ -2,20 +2,24 @@ package com.example.budgetingapp.controllers;
 
 import com.example.budgetingapp.constants.AuthConstants;
 import com.example.budgetingapp.constants.Constants;
-import com.example.budgetingapp.dtos.user.request.UserInitiateResetRequestDto;
+import com.example.budgetingapp.dtos.user.request.UserGetLinkToSetRandomPasswordRequestDto;
 import com.example.budgetingapp.dtos.user.request.UserLoginRequestDto;
 import com.example.budgetingapp.dtos.user.request.UserRegistrationRequestDto;
-import com.example.budgetingapp.dtos.user.request.UserResetRequestDto;
+import com.example.budgetingapp.dtos.user.request.UserSetNewPasswordRequestDto;
 import com.example.budgetingapp.dtos.user.response.UserLoginResponseDto;
 import com.example.budgetingapp.dtos.user.response.UserRegistrationResponseDto;
 import com.example.budgetingapp.exceptions.RegistrationException;
 import com.example.budgetingapp.security.AuthenticationService;
+import com.example.budgetingapp.security.EmailSecretProvider;
 import com.example.budgetingapp.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
+    private final EmailSecretProvider emailSecretProvider;
 
     @Operation(summary = AuthConstants.REGISTER_SUMMARY)
     @ApiResponse(responseCode = Constants.CODE_200, description = Constants.SUCCESSFULLY_REGISTERED)
@@ -49,14 +54,24 @@ public class AuthController {
     }
 
     @PostMapping("/forgot-password")
-    public void initiatePasswordReset(@RequestBody UserInitiateResetRequestDto request) {
+    public void initiatePasswordReset(@RequestBody
+                                          UserGetLinkToSetRandomPasswordRequestDto request) {
         authenticationService.initiatePasswordReset(request.email());
     }
 
-    @PostMapping("/password-reset")
-    public UserLoginResponseDto resetPassword(@RequestBody
-                                                  UserResetRequestDto userResetRequestDto) {
-        return authenticationService.authenticate(
-                authenticationService.resetPassword(userResetRequestDto));
+    @PostMapping("/reset-password")
+    public String resetPassword(HttpServletRequest httpServletRequest) {
+        return authenticationService.resetPassword(httpServletRequest
+                .getParameter(emailSecretProvider.getEmailSecret()));
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(HttpServletRequest httpServletRequest,
+                                 @RequestBody UserSetNewPasswordRequestDto request) {
+        String bearerToken = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(AuthConstants.BEARER)) {
+            bearerToken = bearerToken.substring(7);
+        }
+        return authenticationService.changePassword(bearerToken, request);
     }
 }
