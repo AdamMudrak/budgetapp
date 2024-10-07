48c0552fed74f1044c56a2ffa3d392bedd45a359
Adam Mudrak <mudrets2000@gmail.com>
2024-10-07 12:46:32 +0300

    Add password mismatch exception

--- src/main/java/com/example/budgetingapp/exceptions/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/budgetingapp/exceptions/CustomGlobalExceptionHandler.java
@@ -74,6 +74,17 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
                 request);
     }
 
+    @ExceptionHandler(PasswordMismatch.class)
+    protected ResponseEntity<Object> handlePasswordMismatch(
+            Exception ex, WebRequest request) {
+        return handleExceptionInternal(
+                ex,
+                ex.getMessage(),
+                new HttpHeaders(),
+                BAD_REQUEST,
+                request);
+    }
+
     private String getErrorMessage(ObjectError e) {
         if (e instanceof FieldError fieldError) {
             String field = fieldError.getField();
