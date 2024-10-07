b0595c5c575a21c4d44dee6e5515f38098b1d063
Adam Mudrak <mudrets2000@gmail.com>
2024-10-07 23:57:11 +0300

    Refactor SecurityConstants

--- src/main/java/com/example/budgetingapp/security/SecurityConstants.java
+++ src/main/java/com/example/budgetingapp/security/SecurityConstants.java
@@ -8,19 +8,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SecurityConstants {
-    public static final String PATH = "http://localhost:8080/auth/password-reset?";
+    public static final String PATH = "http://localhost:8080/auth/reset-password?";
+
+    public static final int RANDOM_PASSWORD_STRENGTH = 25;
     public static final String RANDOM_STRING_BASE =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    public static final String JWT_SECRET = "jwt.secret";
     public static final String ACCESS = "ACCESS";
     public static final String RESET = "RESET";
     public static final String REFRESH = "REFRESH";
+
     public static final String INITIATE_RANDOM_PASSWORD_SUBJECT =
             "Initiate password reset for Moneta";
     public static final String RANDOM_PASSWORD_SUBJECT = "New password for Moneta";
     public static final String INITIATE_RANDOM_PASSWORD_BODY = """
             Good day! This email is to help you
             reset password for your moneta account.
-            Please, use this link token to get a random password.""";
+            Please, use this link to get a random password.""";
     public static final String RANDOM_PASSWORD_BODY = """
             Your new random password:
             """;
