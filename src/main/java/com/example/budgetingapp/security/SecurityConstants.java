95f53376b1e8a278fbb622ed4bda91bdf0d22663
Adam Mudrak <mudrets2000@gmail.com>
2024-10-07 15:55:01 +0300

    Add better constants

--- src/main/java/com/example/budgetingapp/security/SecurityConstants.java
+++ src/main/java/com/example/budgetingapp/security/SecurityConstants.java
@@ -8,10 +8,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SecurityConstants {
+    public static final String PATH = "http://localhost:8080/auth/password-reset?";
+    public static final String RANDOM_STRING_BASE =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     public static final String ACCESS = "ACCESS";
     public static final String RESET = "RESET";
     public static final String REFRESH = "REFRESH";
-    public static final String RESET_PASSWORD_SUBJECT = "Reset password  for Moneta";
+    public static final String INITIATE_RANDOM_PASSWORD_SUBJECT =
+            "Initiate password reset for Moneta";
+    public static final String RANDOM_PASSWORD_SUBJECT = "New password for Moneta";
     public static final String INITIATE_RANDOM_PASSWORD_BODY = """
             Good day! This email is to help you
             reset password for your moneta account.
@@ -19,4 +24,9 @@ public class SecurityConstants {
     public static final String RANDOM_PASSWORD_BODY = """
             Your new random password:
             """;
+    public static final String SUCCESSFUL_RESET_MSG = """
+            Your password has been reset successfully.
+            Check email to see your new random password.
+            We recommend setting a new meaningful password
+            as soon as possible""";
 }
