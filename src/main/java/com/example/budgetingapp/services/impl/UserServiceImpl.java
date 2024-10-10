e8a17ff60e2ce38172ce062ae6ff483a7609385b
Adam Mudrak <mudrets2000@gmail.com>
2024-10-10 16:05:12 +0300

    Improvements

--- src/main/java/com/example/budgetingapp/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/budgetingapp/services/impl/UserServiceImpl.java
@@ -1,24 +1,22 @@
 package com.example.budgetingapp.services.impl;
 
 import static com.example.budgetingapp.constants.security.SecurityConstants.ACTION;
-import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRM_REGISTRATION_BODY;
-import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRM_REGISTRATION_SUBJECT;
+import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRMATION;
 
 import com.example.budgetingapp.dtos.user.request.UserRegistrationRequestDto;
-import com.example.budgetingapp.entities.ActionToken;
+import com.example.budgetingapp.entities.ParamToken;
 import com.example.budgetingapp.entities.Role;
 import com.example.budgetingapp.entities.User;
 import com.example.budgetingapp.exceptions.EntityNotFoundException;
 import com.example.budgetingapp.exceptions.RegistrationException;
 import com.example.budgetingapp.mappers.UserMapper;
-import com.example.budgetingapp.repositories.actiontoken.ActionTokenRepository;
+import com.example.budgetingapp.repositories.paramtoken.ParamTokenRepository;
 import com.example.budgetingapp.repositories.role.RoleRepository;
 import com.example.budgetingapp.repositories.user.UserRepository;
 import com.example.budgetingapp.security.EmailService;
 import com.example.budgetingapp.security.jwtutils.JwtStrategy;
 import com.example.budgetingapp.security.jwtutils.abstraction.JwtAbstractUtil;
 import com.example.budgetingapp.services.UserService;
-import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,7 +27,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private final ActionTokenRepository actionTokenRepository;
+    private final ParamTokenRepository paramTokenRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
     private final JwtStrategy jwtStrategy;
@@ -47,12 +45,7 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(requestDto.password()));
         userRepository.save(user);
 
-        JwtAbstractUtil jwtAbstractUtil = jwtStrategy.getStrategy(ACTION);
-        String token = jwtAbstractUtil.generateToken(user.getUsername());
-        ActionToken actionToken = new ActionToken();
-        actionToken.setActionToken(token);
-        actionTokenRepository.save(actionToken);
-        sendConfirmationEmail(user.getEmail(), actionToken.getActionToken());
+        emailService.sendActionEmail(user.getEmail(), CONFIRMATION);
         return "User is registered successfully. "
                 + "Check your email to confirm registration. "
                 + "Your account will not be available until then";
@@ -67,23 +60,14 @@ public class UserServiceImpl implements UserService {
                         + email + " was not found"));
         user.setEnabled(true);
         userRepository.save(user);
-        Optional<ActionToken> actionToken = actionTokenRepository.findByActionToken(token);
-        if (actionToken.isEmpty()) {
-            throw new EntityNotFoundException("No registration request was found by this link");
-        }
-        actionTokenRepository.deleteById(actionToken.get().getId());
+        ParamToken paramToken = paramTokenRepository.findByActionToken(token).orElseThrow(()
+                -> new EntityNotFoundException("No such request"));
+        paramTokenRepository.deleteById(paramToken.getId());
         return "Your registration is successfully confirmed";
     }
 
     private void assignUserRole(User user) {
         Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER);
         user.setRoles(Set.of(userRole));
-    }
-
-    private void sendConfirmationEmail(String email, String token) {
-        emailService.setSubject(CONFIRM_REGISTRATION_SUBJECT);
-        emailService.setBody(CONFIRM_REGISTRATION_BODY);
-        emailService.sendSimpleEmail(email,
-                emailService.formTextForAction(token));
     }
 }
