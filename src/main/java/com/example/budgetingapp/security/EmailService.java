8ca91fe6560bb67cdbcc696da6b534d04af91c22
Adam Mudrak <mudrets2000@gmail.com>
2024-10-10 15:23:16 +0300

    Improve EmailService

--- src/main/java/com/example/budgetingapp/security/EmailService.java
+++ src/main/java/com/example/budgetingapp/security/EmailService.java
@@ -1,8 +1,18 @@
 package com.example.budgetingapp.security;
 
 import static com.example.budgetingapp.constants.Constants.SPLITERATOR;
-import static com.example.budgetingapp.constants.security.SecurityConstants.PATH_PROPERTY;
+import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRMATION;
+import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRMATION_PATH;
+import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRM_REGISTRATION_BODY;
+import static com.example.budgetingapp.constants.security.SecurityConstants.CONFIRM_REGISTRATION_SUBJECT;
+import static com.example.budgetingapp.constants.security.SecurityConstants.INITIATE_RANDOM_PASSWORD_BODY;
+import static com.example.budgetingapp.constants.security.SecurityConstants.INITIATE_RANDOM_PASSWORD_SUBJECT;
+import static com.example.budgetingapp.constants.security.SecurityConstants.RANDOM_PASSWORD_BODY;
+import static com.example.budgetingapp.constants.security.SecurityConstants.RANDOM_PASSWORD_SUBJECT;
+import static com.example.budgetingapp.constants.security.SecurityConstants.RESET;
+import static com.example.budgetingapp.constants.security.SecurityConstants.RESET_PATH;
 
+import com.example.budgetingapp.exceptions.ActionNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,13 +25,15 @@ import org.springframework.stereotype.Service;
 @Setter
 public class EmailService {
     private final JavaMailSender mailSender;
-    private final EmailSecretProvider emailSecretProvider;
-    @Value(PATH_PROPERTY)
-    private String actionPath;
+    private final EmailLinkParameterProvider emailLinkParameterProvider;
+    @Value(RESET_PATH)
+    private String resetPath;
+    @Value(CONFIRMATION_PATH)
+    private String confirmationPath;
     private String subject;
     private String body;
 
-    public void sendSimpleEmail(String to, String setText) {
+    public void sendEmail(String to, String setText) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
@@ -29,14 +41,33 @@ public class EmailService {
         mailSender.send(message);
     }
 
-    public String formTextForAction(String token) {
+    public String formTextForAction(String email, String actionPath) {
+        emailLinkParameterProvider.formRandomParamTokenPair(email);
         return body + System.lineSeparator() + actionPath
-                + emailSecretProvider.getEmailSecret()
-                + SPLITERATOR + token;
+                + emailLinkParameterProvider.getEmailLinkParameter()
+                + SPLITERATOR + emailLinkParameterProvider.getToken();
     }
 
-    public String formTextForNewPassword(String password) {
-        return body + System.lineSeparator()
-                + password;
+    public void sendActionEmail(String email, String action) {
+        switch (action) {
+            case RESET -> {
+                this.setSubject(INITIATE_RANDOM_PASSWORD_SUBJECT);
+                this.setBody(INITIATE_RANDOM_PASSWORD_BODY);
+                this.sendEmail(email, formTextForAction(email, resetPath));
+            }
+            case CONFIRMATION -> {
+                this.setSubject(CONFIRM_REGISTRATION_SUBJECT);
+                this.setBody(CONFIRM_REGISTRATION_BODY);
+                this.sendEmail(email, formTextForAction(email, confirmationPath));
+            }
+            default -> throw new ActionNotFoundException("Unknown action " + action);
+        }
+    }
+
+    public void sendResetPassword(String email, String randomPassword) {
+        this.setSubject(RANDOM_PASSWORD_SUBJECT);
+        this.setBody(RANDOM_PASSWORD_BODY);
+        String mail = body + System.lineSeparator() + randomPassword;
+        this.sendEmail(email, mail);
     }
 }
