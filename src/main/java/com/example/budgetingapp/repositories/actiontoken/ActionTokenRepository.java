51f5c2b9a33aa3df582f7d69450a3a4950b2a140
Adam Mudrak <mudrets2000@gmail.com>
2024-10-13 21:46:26 +0300

    Add action token table for TelegramAuth

---
+++ src/main/java/com/example/budgetingapp/repositories/actiontoken/ActionTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.budgetingapp.repositories.actiontoken;
+
+import com.example.budgetingapp.entities.ActionToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ActionTokenRepository extends JpaRepository<ActionToken, Long> {
+    @Query("SELECT DISTINCT actionToken "
+            + "FROM ActionToken actionToken "
+            + "WHERE actionToken.actionToken = :actionToken")
+    Optional<ActionToken> findByActionToken(String actionToken);
+}
