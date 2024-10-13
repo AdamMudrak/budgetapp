8c775aa002f51bc993ad7dbc83df9cdd1966dbd1
Adam Mudrak <mudrets2000@gmail.com>
2024-10-13 21:54:23 +0300

    Add required DTOs for bot

--- src/main/java/com/example/budgetingapp/dtos/user/request/TelegramAuthenticationRequestDto.java
+++ src/main/java/com/example/budgetingapp/dtos/user/request/TelegramAuthenticationRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.budgetingapp.dtos.user.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//TODO VALIDATE
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TelegramAuthenticationRequestDto(String firstName,
                                              String lastName,
-                                             String phoneNumber,
-                                             String password) {
+                                             String email,
+                                             String password,
+                                             String token) {
 }
