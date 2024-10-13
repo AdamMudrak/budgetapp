71a5fea81857db634e7a3c72132e3d8dd67a92ec
Adam Mudrak <mudrets2000@gmail.com>
2024-10-13 22:03:09 +0300

    Use username instead of email

--- src/main/java/com/example/budgetingapp/dtos/user/request/TelegramAuthenticationRequestDto.java
+++ src/main/java/com/example/budgetingapp/dtos/user/request/TelegramAuthenticationRequestDto.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TelegramAuthenticationRequestDto(String firstName,
                                              String lastName,
-                                             String email,
+                                             String userName,//phoneNumber
                                              String password,
                                              String token) {
 }
