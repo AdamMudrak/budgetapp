9cfe80233317de34e14f579d9257a5bd3f18135e
Adam Mudrak <mudrets2000@gmail.com>
2024-10-14 12:18:40 +0300

    Don't use interface since no further impls are planned or needed

--- src/main/java/com/example/budgetingapp/security/services/TelegramAuthenticationService.java
+++ src/main/java/com/example/budgetingapp/security/services/TelegramAuthenticationService.java
@@ -1,8 +1,43 @@
 package com.example.budgetingapp.security.services;
 
+import static com.example.budgetingapp.constants.controllers.AuthControllerConstants.SUCCESSFULLY_AUTHENTICATED_VIA_TELEGRAM;
+
 import com.example.budgetingapp.dtos.user.request.TelegramAuthenticationRequestDto;
 import com.example.budgetingapp.dtos.user.response.TelegramAuthenticationResponseDto;
+import com.example.budgetingapp.entities.ActionToken;
+import com.example.budgetingapp.entities.Role;
+import com.example.budgetingapp.entities.User;
+import com.example.budgetingapp.exceptions.EntityNotFoundException;
+import com.example.budgetingapp.mappers.UserMapper;
+import com.example.budgetingapp.repositories.actiontoken.ActionTokenRepository;
+import com.example.budgetingapp.repositories.role.RoleRepository;
+import com.example.budgetingapp.repositories.user.UserRepository;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-public interface TelegramAuthenticationService {
-    TelegramAuthenticationResponseDto register(TelegramAuthenticationRequestDto requestDto);
+@Component
+@RequiredArgsConstructor
+public class TelegramAuthenticationService {
+    private final UserRepository userRepository;
+    private final ActionTokenRepository actionTokenRepository;
+    private final RoleRepository roleRepository;
+    private final UserMapper userMapper;
+
+    public TelegramAuthenticationResponseDto register(TelegramAuthenticationRequestDto requestDto) {
+        ActionToken actionToken = actionTokenRepository.findByActionToken(requestDto.token())
+                .orElseThrow(() ->
+                        new EntityNotFoundException("No such request from telegram bot!"));
+        actionTokenRepository.delete(actionToken);
+        User user = userMapper.toTelegramUser(requestDto);
+        assignUserRole(user);
+        user.setEnabled(true);
+        userRepository.save(user);
+        return new TelegramAuthenticationResponseDto(SUCCESSFULLY_AUTHENTICATED_VIA_TELEGRAM);
+    }
+
+    private void assignUserRole(User user) {
+        Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER);
+        user.setRoles(Set.of(userRole));
+    }
 }
