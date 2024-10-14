cbbdc0ad2617a54595ab3f32963c0bce7e93919e
Adam Mudrak <mudrets2000@gmail.com>
2024-10-14 13:07:25 +0300

    Use more constants

--- src/main/java/com/example/budgetingapp/constants/security/SecurityConstants.java
+++ src/main/java/com/example/budgetingapp/constants/security/SecurityConstants.java
@@ -52,19 +52,28 @@ public class SecurityConstants {
                     + "Your account will not be enabled until then";
     public static final String REGISTERED_AND_CONFIRMED =
             "Your registration is successfully confirmed";
+    public static final String SUCCESS_EMAIL = "An email with reset link has been sent";
+
+    public static final String TOKEN = "${telegram.bot.token}";
+    public static final String BOT_TO_SERVER_URI = "${bot.to.server.uri}";
+    public static final String BOT_NAME = "BudgetApplicationBot";
+    public static final String START = "/start";
+    public static final String STOP = "/stop";
+    public static final String STOPPED_SUCCESS = "The bot has been stopped!";
+    public static final String UNKNOWN_COMMAND = "Unknown command. Please use /start, or /stop.";
     public static final String TELEGRAM_REGISTRATION =
             "To register in our app, you'll need"
-            + System.lineSeparator()
-            + "to share your data with us, specifically:"
-            + System.lineSeparator()
-            + "your phoneNumber number, name, last name"
-            + System.lineSeparator()
-            + "This data is going to be used only in"
-            + System.lineSeparator()
-            + "registration purposes. You'll then receive"
-            + System.lineSeparator()
-            + "a password to login in the app";
-    public static final String TOKEN = "${telegram.bot.token}";
+                    + System.lineSeparator()
+                    + "to share your data with us, specifically:"
+                    + System.lineSeparator()
+                    + "your phone number, name, last name"
+                    + System.lineSeparator()
+                    + "This data is going to be used only in"
+                    + System.lineSeparator()
+                    + "registration purposes. You'll then receive"
+                    + System.lineSeparator()
+                    + "a password to login in the app";
+    public static final String CONTENT_TYPE_HEADER = "Content-Type";
+    public static final String CONTENT_TYPE = "application/json";
 
-    public static final String SUCCESS_EMAIL = "An email with reset link has been sent";
 }
