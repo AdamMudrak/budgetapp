eec191556159e88c30ca6c592a24e3983caff7ed
Adam Mudrak <mudrets2000@gmail.com>
2024-10-14 13:08:27 +0300

    Add JsonAlias

--- src/main/java/com/example/budgetingapp/dtos/user/request/UserRegistrationRequestDto.java
+++ src/main/java/com/example/budgetingapp/dtos/user/request/UserRegistrationRequestDto.java
@@ -6,6 +6,7 @@ import com.example.budgetingapp.constants.dtos.UserDtoConstants;
 import com.example.budgetingapp.validation.email.Email;
 import com.example.budgetingapp.validation.fieldmatch.FieldRegisterMatch;
 import com.example.budgetingapp.validation.password.Password;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
@@ -14,6 +15,7 @@ import jakarta.validation.constraints.Size;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @FieldRegisterMatch
 public record UserRegistrationRequestDto(
+        @JsonAlias("email")
         @Schema(name = UserDtoConstants.EMAIL,
         example = UserDtoConstants.EMAIL_EXAMPLE,
         requiredMode = REQUIRED)
