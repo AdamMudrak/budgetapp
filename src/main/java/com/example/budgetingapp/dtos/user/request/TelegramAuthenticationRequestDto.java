f0cb677c7a7b85d2f7f241bbcafd3b75350f1c1e
Adam Mudrak <mudrets2000@gmail.com>
2024-10-14 15:38:11 +0300

    Delete unneeded comment

--- src/main/java/com/example/budgetingapp/dtos/user/request/TelegramAuthenticationRequestDto.java
+++ src/main/java/com/example/budgetingapp/dtos/user/request/TelegramAuthenticationRequestDto.java
@@ -1,7 +1,6 @@
 package com.example.budgetingapp.dtos.user.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//TODO VALIDATE
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TelegramAuthenticationRequestDto(String firstName,
