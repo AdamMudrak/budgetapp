9ed672be0967b42518735532662d1275ffb2dc72
Adam Mudrak <mudrets2000@gmail.com>
2024-10-24 01:19:43 +0300

    Introduce swagger DTO documentation

--- src/main/java/com/example/budgetingapp/dtos/account/request/SetAccountAsDefaultDto.java
+++ src/main/java/com/example/budgetingapp/dtos/account/request/SetAccountAsDefaultDto.java
@@ -1,4 +1,15 @@
 package com.example.budgetingapp.dtos.account.request;
 
-public record SetAccountAsDefaultDto(String name) {
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import com.example.budgetingapp.constants.dtos.AccountDtoConstants;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record SetAccountAsDefaultDto(
+        @Schema(name = AccountDtoConstants.NAME,
+                example = AccountDtoConstants.NAME_EXAMPLE,
+                requiredMode = REQUIRED)
+        @NotBlank
+        String name) {
 }
--- src/main/java/com/example/budgetingapp/dtos/account/request/UpdateAccountDto.java
+++ src/main/java/com/example/budgetingapp/dtos/account/request/UpdateAccountDto.java
@@ -1,11 +1,21 @@
 package com.example.budgetingapp.dtos.account.request;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import com.example.budgetingapp.constants.dtos.AccountDtoConstants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record UpdateAccountDto(
+        @Schema(name = AccountDtoConstants.CURRENT_NAME,
+                example = AccountDtoConstants.NAME_EXAMPLE,
+                requiredMode = REQUIRED)
         @NotBlank
         String currentName,
+        @Schema(name = AccountDtoConstants.NEW_NAME,
+                example = AccountDtoConstants.NEW_NAME_EXAMPLE,
+                requiredMode = REQUIRED)
         @NotBlank
         String newName){}
