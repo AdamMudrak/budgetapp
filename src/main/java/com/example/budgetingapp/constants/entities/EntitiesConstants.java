00c3726af94af27de3a6f32c25318548cf89dfa7
Adam Mudrak <mudrets2000@gmail.com>
2024-10-25 17:27:48 +0300

    Add needed constants for transactions entities

--- src/main/java/com/example/budgetingapp/constants/entities/EntitiesConstants.java
+++ src/main/java/com/example/budgetingapp/constants/entities/EntitiesConstants.java
@@ -1,7 +1,17 @@
 package com.example.budgetingapp.constants.entities;
 
 public class EntitiesConstants {
+    public static final String EXPENSES = "expenses";
+    public static final String ACCOUNT_ID = "account_id";
+    public static final String EXPENSE_CATEGORY_ID = "expense_category_id";
+    public static final String EXPENSE_CATEGORIES = "expense_categories";
+
+    public static final String INCOMES = "incomes";
+    public static final String INCOME_CATEGORY_ID = "income_category_id";
+    public static final String INCOME_CATEGORIES = "income_categories";
+
     public static final String ACCOUNTS = "accounts";
+    public static final String USER_ID = "user_id";
 
     public static final String ACTION_TOKENS = "action_tokens";
 
@@ -11,7 +21,6 @@ public class EntitiesConstants {
 
     public static final String USERS = "users";
     public static final String USERS_ROLES_JOIN_TABLE = "users_roles";
-    public static final String USER_ID = "user_id";
     public static final String ROLE_ID = "role_id";
     public static final String BOOLEAN_TO_INT = "TINYINT(1)";
 
