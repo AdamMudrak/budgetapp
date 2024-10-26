c927ef7967731b896abc463d18e68f1642463566
Adam Mudrak <mudrets2000@gmail.com>
2024-10-26 17:43:07 +0300

    Add methods required for making sure user don't access what is not theirs

--- src/main/java/com/example/budgetingapp/repositories/account/AccountRepository.java
+++ src/main/java/com/example/budgetingapp/repositories/account/AccountRepository.java
@@ -8,9 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> getAllByUserId(Long userId);
 
+    boolean existsByIdAndUserId(Long id, Long userId);
+
     boolean existsByUserIdAndName(Long userId, String name);
 
     boolean existsByUserIdAndByDefault(Long userId, boolean byDefault);
+
+    Optional<Account> findByIdAndUserId(Long id, Long userId);
 
     Optional<Account> findByUserIdAndName(Long userId, String name);
 
