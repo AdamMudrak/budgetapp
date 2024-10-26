49b2cf83c84a57c144f1d653cb952c161477cd42
Adam Mudrak <mudrets2000@gmail.com>
2024-10-26 16:48:40 +0300

    Use category mapper

---
+++ src/main/java/com/example/budgetingapp/mappers/CategoryMapper.java
@@ -0,0 +1,27 @@
+package com.example.budgetingapp.mappers;
+
+import com.example.budgetingapp.config.MapperConfig;
+import com.example.budgetingapp.dtos.transactions.request.CreateCategoryDto;
+import com.example.budgetingapp.dtos.transactions.response.ResponseCategoryDto;
+import com.example.budgetingapp.entities.transactions.expenses.ExpenseCategory;
+import com.example.budgetingapp.entities.transactions.incomes.IncomeCategory;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface CategoryMapper {
+    IncomeCategory toIncomeCategory(CreateCategoryDto createCategoryDto);
+
+    @Mapping(source = "user.id", target = "userId")
+    ResponseCategoryDto toIncomeCategoryDto(IncomeCategory incomeCategory);
+
+    @Mapping(source = "user.id", target = "userId")
+    ResponseCategoryDto toExpenseCategoryDto(ExpenseCategory expenseCategory);
+
+    @Mapping(source = "user.id", target = "userId")
+    List<ResponseCategoryDto> toIncomeCategoryDtoList(List<IncomeCategory> accounts);
+
+    @Mapping(source = "user.id", target = "userId")
+    List<ResponseCategoryDto> toExpenseCategoryDtoList(List<ExpenseCategory> accounts);
+}
