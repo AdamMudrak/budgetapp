6f266be2c76a49e4c79ad886862132641d966a97
Adam Mudrak <mudrets2000@gmail.com>
2024-10-26 17:32:52 +0300

    Change order of values

--- src/main/java/com/example/budgetingapp/dtos/transactions/request/RequestTransactionDto.java
+++ src/main/java/com/example/budgetingapp/dtos/transactions/request/RequestTransactionDto.java
@@ -1,10 +1,9 @@
 package com.example.budgetingapp.dtos.transactions.request;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public record RequestTransactionDto(String comment,
-                                    Long accountId,
                                     BigDecimal amount,
-                                    LocalDate transactionDate,
-                                    Long expenseCategoryId){}
+                                    String transactionDate, //TODO mark it down somehow
+                                    Long accountId,
+                                    Long categoryId){}
