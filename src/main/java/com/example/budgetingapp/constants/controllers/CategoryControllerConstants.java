8c1deffaea6a2a9083998c98d5edb9a5b9dfe1f0
Adam Mudrak <mudrets2000@gmail.com>
2024-10-26 16:28:10 +0300

    Add well-documented category controller

---
+++ src/main/java/com/example/budgetingapp/constants/controllers/CategoryControllerConstants.java
@@ -0,0 +1,31 @@
+package com.example.budgetingapp.constants.controllers;
+
+public class CategoryControllerConstants {
+    public static final String CATEGORY_API_NAME = "Category API";
+    public static final String CATEGORY_API_DESCRIPTION = """
+            Here you'll find a comprehensive overview
+            of how to work with categories of transactions
+            in this app""";
+    public static final String CATEGORIES = "/categories";
+
+    public static final String ADD_CATEGORY = "/add-category";
+    public static final String GET_ALL_CATEGORIES = "/get-all-categories";
+    public static final String UPDATE_CATEGORY_BY_ID = "/update-category/{categoryId}";
+    public static final String DELETE_CATEGORY_BY_ID = "/delete-category/{categoryId}";
+
+    public static final String ADD_CATEGORY_SUMMARY = "Add category";
+    public static final String SUCCESSFULLY_ADDED_CATEGORY =
+            "Successfully added a category";
+
+    public static final String UPDATE_CATEGORY_SUMMARY = "Update category";
+    public static final String SUCCESSFULLY_UPDATE_CATEGORY =
+            "Successfully updated a category";
+
+    public static final String DELETE_CATEGORY_SUMMARY = "Delete category";
+    public static final String SUCCESSFULLY_DELETE_CATEGORY =
+            "Successfully deleted a category";
+
+    public static final String GET_ALL_CATEGORIES_SUMMARY = "Get all user's categories";
+    public static final String SUCCESSFULLY_RETRIEVED_CATEGORIES =
+            "Successfully retrieved categories";
+}
