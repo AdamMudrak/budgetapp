a88d818ac30caad4874a71d55b1a9ca12d135dda
Adam Mudrak <mudrets2000@gmail.com>
2024-10-28 16:23:48 +0200

    Add timeout

--- src/main/java/com/example/budgetingapp/telegramregistrationbot/BudgetAppBot.java
+++ src/main/java/com/example/budgetingapp/telegramregistrationbot/BudgetAppBot.java
@@ -28,6 +28,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -45,6 +47,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class BudgetAppBot extends TelegramLongPollingBot {
+    private static final Long TIME_OUT = 20L;
     private static final Set<Long> stoppedUserIds = new HashSet<>();
     private static final HttpClient httpClient = HttpClient.newHttpClient();
     private final RandomStringUtil randomStringUtil;
@@ -171,6 +174,7 @@ public class BudgetAppBot extends TelegramLongPollingBot {
                     .uri(URI.create(botToServerRequestUri))
                     .header(CONTENT_TYPE_HEADER, CONTENT_TYPE)
                     .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .timeout(Duration.of(TIME_OUT, ChronoUnit.SECONDS))
                     .build();
         HttpResponse<String> response;
         try {
