f5172bae4cc83c0c3d32f9f810c6bcd9db8ea9e6
Adam Mudrak <mudrets2000@gmail.com>
2024-10-30 17:47:18 +0200

    Add proper documentation for FilterDto

--- src/main/java/com/example/budgetingapp/constants/dtos/TransactionDtoConstants.java
+++ src/main/java/com/example/budgetingapp/constants/dtos/TransactionDtoConstants.java
@@ -9,4 +9,17 @@ public class TransactionDtoConstants {
 
     public static final String DATE = "transactionDate";
     public static final String DATE_EXAMPLE = "2024-10-29";
+
+    public static final String ACCOUNT_ID = "accountId";
+    public static final String ACCOUNT_ID_EXAMPLE = "3";
+
+    public static final String FROM_DATE = "fromDate";
+    public static final String FROM_DATE_EXAMPLE = "2024-10-29";
+
+    public static final String TO_DATE = "toDate";
+    public static final String TO_DATE_EXAMPLE = "2024-10-31";
+    public static final String DATE_DESCRIPTION =
+            "date format should be: YYYY / YYYY-MM / YYYY-MM-dd";
+    public static final String CATEGORY_IDS = "categoryIds";
+    public static final String CATEGORY_IDS_EXAMPLE = "[\"1\", \"2\"]";
 }
