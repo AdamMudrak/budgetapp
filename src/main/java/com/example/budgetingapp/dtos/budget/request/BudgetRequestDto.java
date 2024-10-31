b130c15d09c0c978df0411d4547801b0448f359a
Adam Mudrak <mudrets2000@gmail.com>
2024-11-01 01:17:34 +0200

    Add required DTOs

---
+++ src/main/java/com/example/budgetingapp/dtos/budget/request/BudgetRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.budgetingapp.dtos.budget.request;
+
+import com.example.budgetingapp.validation.date.Date;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.util.Set;
+
+public record BudgetRequestDto(
+        @NotBlank
+        String name,
+        @NotBlank
+        @Date
+        String fromDate,
+        @NotBlank
+        @Date
+        String toDate,
+        @NotNull
+        @Positive
+        @Digits(integer = 9, fraction = 0)
+        Set<Long> categoryIds,
+        @NotNull
+        @Positive
+        @Digits(integer = 9, fraction = 2)
+        BigDecimal limit) {}
---
+++ src/main/java/com/example/budgetingapp/dtos/budget/response/BudgetResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.budgetingapp.dtos.budget.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BudgetResponseDto {
+    private String name;
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private Set<Long> categoryIds;
+    private BigDecimal limit;
+    private BigDecimal currentSum;
+    private boolean isExceed;
+}
