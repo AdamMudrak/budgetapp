2f72398d646e1c7016a2bdebaa41b7a34169f58b
Adam Mudrak <mudrets2000@gmail.com>
2024-10-31 14:31:00 +0200

    Fully document target controller

--- src/main/java/com/example/budgetingapp/constants/controllers/TargetControllerConstants.java
+++ src/main/java/com/example/budgetingapp/constants/controllers/TargetControllerConstants.java
@@ -8,5 +8,27 @@ public class TargetControllerConstants {
             retrieving all the money to one of the
             accounts in this app.
             """;
+
     public static final String TARGETS = "/targets";
+
+    public static final String ADD_TARGET = "/add-target";
+    public static final String ADD_TARGET_SUMMARY = "Add target";
+    public static final String SUCCESSFULLY_ADDED_TARGET =
+            "Successfully added a target";
+
+    public static final String REPLENISH_TARGET = "/replenish-target";
+    public static final String REPLENISH_SUMMARY = "Replenish target";
+    public static final String SUCCESSFULLY_REPLENISHED_TARGET =
+            "Successfully replenished a target";
+
+    public static final String GET_ALL_TARGETS = "/get-all-targets";
+    public static final String GET_ALL_TARGETS_SUMMARY = "Retrieved all targets";
+    public static final String SUCCESSFULLY_RETRIEVED_TARGETS =
+            "Successfully retrieved all targets";
+
+    public static final String DESTROY_TARGET = "/destroy-target";
+    public static final String DESTROY_TARGET_SUMMARY =
+            "Destroy a target, retrieve money to one of you accounts";
+    public static final String SUCCESSFULLY_DESTROYED_TARGET =
+            "Successfully destroy a target and automatically replenish the account chosen";
 }
--- src/main/java/com/example/budgetingapp/controllers/transfers/TargetController.java
+++ src/main/java/com/example/budgetingapp/controllers/transfers/TargetController.java
@@ -1,6 +1,23 @@
 package com.example.budgetingapp.controllers.transfers;
 
+import static com.example.budgetingapp.constants.Constants.CODE_200;
+import static com.example.budgetingapp.constants.Constants.CODE_201;
+import static com.example.budgetingapp.constants.Constants.CODE_204;
+import static com.example.budgetingapp.constants.Constants.CODE_400;
+import static com.example.budgetingapp.constants.Constants.INVALID_ENTITY_VALUE;
 import static com.example.budgetingapp.constants.Constants.ROLE_USER;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.ADD_TARGET;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.ADD_TARGET_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.DESTROY_TARGET;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.DESTROY_TARGET_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.GET_ALL_TARGETS;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.GET_ALL_TARGETS_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.REPLENISH_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.REPLENISH_TARGET;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.SUCCESSFULLY_ADDED_TARGET;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.SUCCESSFULLY_DESTROYED_TARGET;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.SUCCESSFULLY_REPLENISHED_TARGET;
+import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.SUCCESSFULLY_RETRIEVED_TARGETS;
 import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.TARGETS;
 import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.TARGET_API_DESCRIPTION;
 import static com.example.budgetingapp.constants.controllers.TargetControllerConstants.TARGET_API_NAME;
@@ -11,10 +28,13 @@ import com.example.budgetingapp.dtos.transfers.request.TargetTransactionRequestD
 import com.example.budgetingapp.dtos.transfers.response.TargetTransactionResponseDto;
 import com.example.budgetingapp.entities.User;
 import com.example.budgetingapp.services.TargetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +42,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @PreAuthorize(ROLE_USER)
@@ -32,27 +53,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class TargetController {
     private final TargetService targetService;
 
-    @PostMapping
+    @Operation(summary = ADD_TARGET_SUMMARY)
+    @ApiResponse(responseCode = CODE_201, description = SUCCESSFULLY_ADDED_TARGET)
+    @ApiResponse(responseCode = CODE_400, description = INVALID_ENTITY_VALUE)
+    @PostMapping(ADD_TARGET)
+    @ResponseStatus(HttpStatus.CREATED)
     public TargetTransactionResponseDto addTarget(@AuthenticationPrincipal User user,
                                               @RequestBody TargetTransactionRequestDto requestDto) {
         return targetService.saveTarget(user.getId(), requestDto);
     }
 
-    @PostMapping("/replenish")
+    @Operation(summary = REPLENISH_SUMMARY)
+    @ApiResponse(responseCode = CODE_200, description = SUCCESSFULLY_REPLENISHED_TARGET)
+    @ApiResponse(responseCode = CODE_400, description = INVALID_ENTITY_VALUE)
+    @PostMapping(REPLENISH_TARGET)
     public TargetTransactionResponseDto replenishTarget(@AuthenticationPrincipal User user,
                                           @RequestBody ReplenishTargetRequestDto requestDto) {
         return targetService.replenishTarget(user.getId(), requestDto);
     }
 
-    @DeleteMapping
-    public void deleteTarget(@AuthenticationPrincipal User user,
-                                @RequestBody DeleteTargetRequestDto requestDto) {
-        targetService.deleteByTargetId(user.getId(), requestDto);
-    }
-
-    @GetMapping
+    @Operation(summary = GET_ALL_TARGETS_SUMMARY)
+    @ApiResponse(responseCode = CODE_200, description = SUCCESSFULLY_RETRIEVED_TARGETS)
+    @GetMapping(GET_ALL_TARGETS)
     public List<TargetTransactionResponseDto> getAllTargets(@AuthenticationPrincipal User user,
                                                             Pageable pageable) {
         return targetService.getAllTargets(user.getId(), pageable);
+    }
+
+    @Operation(summary = DESTROY_TARGET_SUMMARY)
+    @ApiResponse(responseCode = CODE_204, description = SUCCESSFULLY_DESTROYED_TARGET)
+    @DeleteMapping(DESTROY_TARGET)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTarget(@AuthenticationPrincipal User user,
+                             @RequestBody DeleteTargetRequestDto requestDto) {
+        targetService.deleteByTargetId(user.getId(), requestDto);
     }
 }
