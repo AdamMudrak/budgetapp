899aaabe57228ba87188e6c72890a66258229f94
Adam Mudrak <mudrets2000@gmail.com>
2024-11-01 13:27:23 +0200

    Fix mistake with name of functions

--- src/main/java/com/example/budgetingapp/repositories/budget/BudgetRepository.java
+++ src/main/java/com/example/budgetingapp/repositories/budget/BudgetRepository.java
@@ -3,16 +3,15 @@ package com.example.budgetingapp.repositories.budget;
 import com.example.budgetingapp.entities.Budget;
 import jakarta.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
-    boolean existByIdAndUserId(Long id, Long userId);
+    boolean existsByIdAndUserId(Long id, Long userId);
 
-    boolean existByNameAndUserId(String name, Long userId);
+    boolean existsByNameAndUserId(String name, Long userId);
 
-    Optional<Budget> findByIsTopLevelBudget(boolean isTopLevelBudget);
+    //TODO Optional<Budget> findByIsTopLevelBudget(boolean isTopLevelBudget);
 
     List<Budget> findAllByUserId(Long userId);
 
