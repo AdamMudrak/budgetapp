690a858bb7f9e4c15bcdc0102879deea02962f89
Adam Mudrak <mudrets2000@gmail.com>
2024-11-03 22:03:14 +0200

    Add good documentation for budget API

---
+++ src/main/java/com/example/budgetingapp/constants/controllers/BudgetControllerConstants.java
@@ -0,0 +1,29 @@
+package com.example.budgetingapp.constants.controllers;
+
+public class BudgetControllerConstants {
+    public static final String BUDGET_API_NAME = "Budget API";
+    public static final String BUDGET_API_DESCRIPTION = """
+            Here you'll find a comprehensive overview
+            of how to add, get and delete user's
+            budgets for specific categories
+            in this app.
+            """;
+    public static final String BUDGET = "/budgets";
+    public static final String ADD_BUDGET = "/add-budget";
+    public static final String GET_TOP_LEVEL_BUDGET = "/get-top-level-budget";
+    public static final String GET_ALL_BUDGETS = "/get-all-budgets";
+    public static final String DELETE_BUDGET_BY_ID = "/delete-budget/{budgetId}";
+
+    public static final String ADD_BUDGET_SUMMARY = "Add a new user's budget";
+    public static final String SUCCESSFULLY_ADDED = "Successfully added a budget";
+
+    public static final String GET_ALL_BUDGETS_SUMMARY = "Get all user's budgets";
+    public static final String SUCCESSFULLY_RETRIEVED = "Successfully retrieved budgets";
+
+    public static final String GET_MAIN_BUDGET_SUMMARY = "Get user's main budget for all periods";
+    public static final String SUCCESSFULLY_RETRIEVED_MAIN_BUDGET =
+            "Successfully retrieved main budget";
+
+    public static final String DELETE_BUDGET_BY_ID_SUMMARY = "Delete a user's budget";
+    public static final String SUCCESSFULLY_DELETED = "Successfully deleted a budget";
+}
--- src/main/java/com/example/budgetingapp/controllers/BudgetController.java
+++ src/main/java/com/example/budgetingapp/controllers/BudgetController.java
@@ -1,13 +1,36 @@
 package com.example.budgetingapp.controllers;
 
+import static com.example.budgetingapp.constants.Constants.CODE_200;
+import static com.example.budgetingapp.constants.Constants.CODE_201;
+import static com.example.budgetingapp.constants.Constants.CODE_400;
+import static com.example.budgetingapp.constants.Constants.INVALID_ENTITY_VALUE;
 import static com.example.budgetingapp.constants.Constants.ROLE_USER;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.ADD_BUDGET;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.ADD_BUDGET_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.BUDGET;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.BUDGET_API_DESCRIPTION;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.BUDGET_API_NAME;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.DELETE_BUDGET_BY_ID;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.DELETE_BUDGET_BY_ID_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.GET_ALL_BUDGETS;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.GET_ALL_BUDGETS_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.GET_MAIN_BUDGET_SUMMARY;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.GET_TOP_LEVEL_BUDGET;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.SUCCESSFULLY_ADDED;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.SUCCESSFULLY_DELETED;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.SUCCESSFULLY_RETRIEVED;
+import static com.example.budgetingapp.constants.controllers.BudgetControllerConstants.SUCCESSFULLY_RETRIEVED_MAIN_BUDGET;
 
 import com.example.budgetingapp.dtos.budgets.request.BudgetRequestDto;
 import com.example.budgetingapp.dtos.budgets.response.BudgetResponseDto;
 import com.example.budgetingapp.entities.User;
 import com.example.budgetingapp.services.BudgetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,32 +39,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @PreAuthorize(ROLE_USER)
 @RequiredArgsConstructor
+@Tag(name = BUDGET_API_NAME, description = BUDGET_API_DESCRIPTION)
 @RestController
-@RequestMapping("/budgets")
+@RequestMapping(BUDGET)
 public class BudgetController {
     private final BudgetService budgetService;
 
-    @PostMapping("add-budget")
+    @Operation(summary = ADD_BUDGET_SUMMARY)
+    @ApiResponse(responseCode = CODE_201, description = SUCCESSFULLY_ADDED)
+    @ApiResponse(responseCode = CODE_400, description = INVALID_ENTITY_VALUE)
+    @PostMapping(ADD_BUDGET)
+    @ResponseStatus(HttpStatus.CREATED)
     public BudgetResponseDto addBudget(@AuthenticationPrincipal User user,
                                        @RequestBody BudgetRequestDto budgetRequestDto) {
         return budgetService.saveBudget(user.getId(), budgetRequestDto);
     }
 
-    @GetMapping("get-top-level-budget")
+    @Operation(summary = GET_MAIN_BUDGET_SUMMARY)
+    @ApiResponse(responseCode = CODE_200, description = SUCCESSFULLY_RETRIEVED_MAIN_BUDGET)
+    @GetMapping(GET_TOP_LEVEL_BUDGET)
     public BudgetResponseDto getTopLevelBudget(@AuthenticationPrincipal User user) {
         return budgetService.updateAndGetMainBudgetByUserId(user.getId());
     }
 
-    @GetMapping("get-all-budgets")
+    @Operation(summary = GET_ALL_BUDGETS_SUMMARY)
+    @ApiResponse(responseCode = CODE_200, description = SUCCESSFULLY_RETRIEVED)
+    @GetMapping(GET_ALL_BUDGETS)
     public List<BudgetResponseDto> getAllBudgets(@AuthenticationPrincipal User user) {
         return budgetService.updateAndGetAllBudgetsWithoutTopLevel(user.getId());
     }
 
-    @DeleteMapping("delete-budget/{budgetId}")
+    @Operation(summary = DELETE_BUDGET_BY_ID_SUMMARY)
+    @ApiResponse(responseCode = CODE_201, description = SUCCESSFULLY_DELETED)
+    @DeleteMapping(DELETE_BUDGET_BY_ID)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBudget(@AuthenticationPrincipal User user,
                                           @PathVariable Long budgetId) {
         budgetService.deleteBudgetById(user.getId(), budgetId);
