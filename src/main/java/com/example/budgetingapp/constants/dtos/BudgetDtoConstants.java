5d71d83618ba85b18faa5d899c609dbeb48c38c1
Adam Mudrak <mudrets2000@gmail.com>
2024-11-04 01:53:20 +0200

    Add @Schema documentation for BudgetRequestDto

---
+++ src/main/java/com/example/budgetingapp/constants/dtos/BudgetDtoConstants.java
@@ -0,0 +1,18 @@
+package com.example.budgetingapp.constants.dtos;
+
+public class BudgetDtoConstants {
+    public static final String NAME = "name";
+    public static final String NAME_EXAMPLE = "New budget";
+    public static final String FROM_DATE = "fromDate";
+    public static final String FROM_DATE_EXAMPLE = "2024-10-29";
+    public static final String TO_DATE = "toDate";
+    public static final String TO_DATE_EXAMPLE = "2024-10-31";
+    public static final String DATE_DESCRIPTION =
+            "date format should be: YYYY-MM-dd";
+
+    public static final String CATEGORY_IDS = "categoryIds";
+    public static final String CATEGORY_IDS_EXAMPLE = "[\"1\", \"2\"]";
+
+    public static final String LIMIT_SUM = "limitSum";
+    public static final String LIMIT_SUM_EXAMPLE = "999.99";
+}
--- src/main/java/com/example/budgetingapp/dtos/budgets/request/BudgetRequestDto.java
+++ src/main/java/com/example/budgetingapp/dtos/budgets/request/BudgetRequestDto.java
@@ -1,7 +1,11 @@
 package com.example.budgetingapp.dtos.budgets.request;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import com.example.budgetingapp.constants.dtos.BudgetDtoConstants;
 import com.example.budgetingapp.validation.date.Date;
 import com.example.budgetingapp.validation.date.todateafterfromdate.BudgetToDateAfterFromDate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,18 +15,35 @@ import java.util.Set;
 
 @BudgetToDateAfterFromDate
 public record BudgetRequestDto(
+        @Schema(name = BudgetDtoConstants.NAME,
+                example = BudgetDtoConstants.NAME_EXAMPLE,
+                requiredMode = REQUIRED)
         @NotBlank
         String name,
+        @Schema(name = BudgetDtoConstants.FROM_DATE,
+                example = BudgetDtoConstants.FROM_DATE_EXAMPLE,
+                description = BudgetDtoConstants.DATE_DESCRIPTION,
+                requiredMode = REQUIRED)
         @NotBlank
         @Date
         String fromDate,
+        @Schema(name = BudgetDtoConstants.TO_DATE,
+                example = BudgetDtoConstants.TO_DATE_EXAMPLE,
+                description = BudgetDtoConstants.DATE_DESCRIPTION,
+                requiredMode = REQUIRED)
         @NotBlank
         @Date
         String toDate,
+        @Schema(name = BudgetDtoConstants.CATEGORY_IDS,
+                example = BudgetDtoConstants.CATEGORY_IDS_EXAMPLE,
+                requiredMode = REQUIRED)
         @NotNull
         @Positive
         @Digits(integer = 9, fraction = 0)
         Set<Long> categoryIds,
+        @Schema(name = BudgetDtoConstants.LIMIT_SUM,
+                example = BudgetDtoConstants.LIMIT_SUM_EXAMPLE,
+                requiredMode = REQUIRED)
         @NotNull
         @Positive
         @Digits(integer = 9, fraction = 2)
