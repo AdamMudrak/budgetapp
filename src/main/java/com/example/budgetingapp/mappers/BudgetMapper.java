2f092c0db688c2ed4d39e335466642cd289127b8
Adam Mudrak <mudrets2000@gmail.com>
2024-11-08 21:30:41 +0200

    Optimize BudgetMapper

--- src/main/java/com/example/budgetingapp/mappers/BudgetMapper.java
+++ src/main/java/com/example/budgetingapp/mappers/BudgetMapper.java
@@ -3,50 +3,15 @@ package com.example.budgetingapp.mappers;
 import com.example.budgetingapp.config.MapperConfig;
 import com.example.budgetingapp.dtos.budgets.request.BudgetRequestDto;
 import com.example.budgetingapp.dtos.budgets.response.BudgetResponseDto;
-import com.example.budgetingapp.dtos.budgets.response.TopLevelBudgetResponseDto;
 import com.example.budgetingapp.entities.Budget;
-import com.example.budgetingapp.entities.categories.ExpenseCategory;
-import com.example.budgetingapp.exceptions.notfoundexceptions.EntityNotFoundException;
-import java.util.Set;
-import java.util.stream.Collectors;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BudgetMapper {
-
-    @Mapping(target = "expenseCategories", ignore = true)
+    @Mapping(source = "categoryId", target = "expenseCategory.id")
     Budget toBudget(BudgetRequestDto budgetRequestDto);
 
-    @AfterMapping
-    default void setCategory(@MappingTarget Budget budget, BudgetRequestDto budgetRequestDto) {
-        ExpenseCategory expenseCategory = new ExpenseCategory();
-        expenseCategory.setId(budgetRequestDto.categoryId());
-        budget.setExpenseCategories(Set.of(expenseCategory));
-    }
-
-    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(source = "expenseCategory.id", target = "categoryId")
     BudgetResponseDto toBudgetDto(Budget budget);
-
-    @AfterMapping
-    default void setCategoryId(@MappingTarget BudgetResponseDto budgetResponseDto, Budget budget) {
-        budgetResponseDto.setCategoryId(budget.getExpenseCategories().stream()
-                .map(ExpenseCategory::getId).findFirst()
-                .orElseThrow(() -> new EntityNotFoundException(
-                        "No category found for budget " + budget.getId())));
-    }
-
-    @Mapping(target = "categoryIds", ignore = true)
-    TopLevelBudgetResponseDto toTopLevelBudgetDto(Budget budget);
-
-    @AfterMapping
-    default void setCategoryIds(@MappingTarget TopLevelBudgetResponseDto budgetResponseDto,
-                                Budget budget) {
-        Set<Long> categoryIds = budget.getExpenseCategories().stream()
-                .map(ExpenseCategory::getId)
-                .collect(Collectors.toSet());
-        budgetResponseDto.setCategoryIds(categoryIds);
-    }
 }
