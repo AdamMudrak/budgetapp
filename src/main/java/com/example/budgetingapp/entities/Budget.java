e8385dbd18e04a90d688be191cf11c90be04e190
Adam Mudrak <mudrets2000@gmail.com>
2024-11-08 21:15:00 +0200

    Delete budget-expense-category as unnecessary, include category id in as element of budget entity

--- src/main/java/com/example/budgetingapp/entities/Budget.java
+++ src/main/java/com/example/budgetingapp/entities/Budget.java
@@ -2,6 +2,7 @@ package com.example.budgetingapp.entities;
 
 import static com.example.budgetingapp.constants.entities.EntitiesConstants.BOOLEAN_TO_INT;
 import static com.example.budgetingapp.constants.entities.EntitiesConstants.BUDGETS;
+import static com.example.budgetingapp.constants.entities.EntitiesConstants.EXPENSE_CATEGORY_ID;
 import static com.example.budgetingapp.constants.entities.EntitiesConstants.USER_ID;
 
 import com.example.budgetingapp.entities.categories.ExpenseCategory;
@@ -12,14 +13,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -37,12 +34,9 @@ public class Budget {
     private LocalDate fromDate;
     @Column(nullable = false)
     private LocalDate toDate;
-    @ManyToMany
-    @JoinTable(
-            name = "budgets_expense_categories",
-            joinColumns = @JoinColumn(name = "budget_id"),
-            inverseJoinColumns = @JoinColumn(name = "expense_category_id"))
-    private Set<ExpenseCategory> expenseCategories = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = EXPENSE_CATEGORY_ID, nullable = false)
+    private ExpenseCategory expenseCategory;
     @Column(nullable = false)
     private BigDecimal limitSum;
     @Column(nullable = false)
