f02b92134357532f43b510e221bd0d604eb6d4ae
Adam Mudrak <mudrets2000@gmail.com>
2024-11-08 21:49:00 +0200

    Delete unnecessary @Query

--- src/main/java/com/example/budgetingapp/repositories/actiontoken/ActionTokenRepository.java
+++ src/main/java/com/example/budgetingapp/repositories/actiontoken/ActionTokenRepository.java
@@ -3,11 +3,7 @@ package com.example.budgetingapp.repositories.actiontoken;
 import com.example.budgetingapp.entities.tokens.ActionToken;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ActionTokenRepository extends JpaRepository<ActionToken, Long> {
-    @Query("SELECT actionToken "
-            + "FROM ActionToken actionToken "
-            + "WHERE actionToken.actionToken = :actionToken")
     Optional<ActionToken> findByActionToken(String actionToken);
 }
