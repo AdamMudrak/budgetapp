83e23649c08a2e7d5916647f3c32980dcdeb7a12
Adam Mudrak <mudrets2000@gmail.com>
2024-11-08 21:15:30 +0200

    Delete unnecessary functions on repo level

--- src/main/java/com/example/budgetingapp/repositories/budget/BudgetRepository.java
+++ src/main/java/com/example/budgetingapp/repositories/budget/BudgetRepository.java
@@ -3,7 +3,6 @@ package com.example.budgetingapp.repositories.budget;
 import com.example.budgetingapp.entities.Budget;
 import jakarta.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -21,13 +20,9 @@ public interface BudgetRepository extends JpaRepository<Budget, Long> {
 
     boolean existsByNameAndUserId(String name, Long userId);
 
-    Optional<Budget> findByIdAndUserId(Long id, Long userId);
-
-    Optional<Budget> findByUserIdAndIsTopLevelBudget(Long userId, boolean isTopLevelBudget);
+    boolean existsByIdAndUserId(Long id, Long userId);
 
     List<Budget> findAllByUserId(Long userId);
-
-    List<Budget> findAllByUserIdAndIsTopLevelBudget(Long userId, boolean isTopLevelBudget);
 
     @Transactional
     @Modifying
