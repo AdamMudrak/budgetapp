6785f0fe930f228e9f8e5d0fe14e2108299e05da
Adam Mudrak <mudrets2000@gmail.com>
2024-11-12 14:03:23 +0200

    Use appropriate data types for filter DTO

--- src/main/java/com/example/budgetingapp/dtos/transactions/request/FilterTransactionsDto.java
+++ src/main/java/com/example/budgetingapp/dtos/transactions/request/FilterTransactionsDto.java
@@ -5,13 +5,17 @@ import com.example.budgetingapp.validation.date.Date;
 import com.example.budgetingapp.validation.date.todateafterfromdate.FilterToDateAfterFromDate;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Set;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @FilterToDateAfterFromDate
 public record FilterTransactionsDto(
         @Schema(name = TransactionDtoConstants.ACCOUNT_ID,
                 example = TransactionDtoConstants.ACCOUNT_ID_EXAMPLE)
-        String accountId,
+        Long accountId,
+        @Schema(name = TransactionDtoConstants.CATEGORY_IDS,
+                example = TransactionDtoConstants.CATEGORY_IDS_EXAMPLE)
+        Set<Long> categoryIds,
         @Schema(name = TransactionDtoConstants.FROM_DATE,
                 example = TransactionDtoConstants.FROM_DATE_EXAMPLE,
                 description = TransactionDtoConstants.DATE_DESCRIPTION)
@@ -21,8 +25,5 @@ public record FilterTransactionsDto(
                 example = TransactionDtoConstants.TO_DATE_EXAMPLE,
                 description = TransactionDtoConstants.DATE_DESCRIPTION)
         @Date
-        String toDate,
-        @Schema(name = TransactionDtoConstants.CATEGORY_IDS,
-                example = TransactionDtoConstants.CATEGORY_IDS_EXAMPLE)
-        String[] categoryIds) {
+        String toDate) {
 }
