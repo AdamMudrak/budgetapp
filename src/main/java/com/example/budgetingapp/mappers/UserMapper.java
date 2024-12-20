8bdfb7a8187f76cafcfab22ae188e2f58886ad97
Adam Mudrak <mudrets2000@gmail.com>
2024-12-20 03:04:48 +0200

    Add mapper for transforming email \ telegram request to inner dto request

--- src/main/java/com/example/budgetingapp/mappers/UserMapper.java
+++ src/main/java/com/example/budgetingapp/mappers/UserMapper.java
@@ -3,6 +3,9 @@ package com.example.budgetingapp.mappers;
 import com.example.budgetingapp.config.MapperConfig;
 import com.example.budgetingapp.dtos.users.request.TelegramAuthenticationRequestDto;
 import com.example.budgetingapp.dtos.users.request.UserRegistrationRequestDto;
+import com.example.budgetingapp.dtos.users.request.userloginrequestdtos.InnerUserLoginRequestDto;
+import com.example.budgetingapp.dtos.users.request.userloginrequestdtos.UserEmailLoginRequestDto;
+import com.example.budgetingapp.dtos.users.request.userloginrequestdtos.UserTelegramLoginRequestDto;
 import com.example.budgetingapp.entities.User;
 import org.mapstruct.Mapper;
 
@@ -11,4 +14,9 @@ public interface UserMapper {
     User toUser(UserRegistrationRequestDto userRegistrationRequestDto);
 
     User toTelegramUser(TelegramAuthenticationRequestDto authenticationRequestDto);
+
+    InnerUserLoginRequestDto toInnerUserDto(
+            UserTelegramLoginRequestDto userTelegramLoginRequestDto);
+
+    InnerUserLoginRequestDto toInnerUserDto(UserEmailLoginRequestDto userEmailLoginRequestDto);
 }
