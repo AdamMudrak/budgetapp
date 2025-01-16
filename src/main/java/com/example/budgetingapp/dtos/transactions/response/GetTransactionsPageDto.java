96b857c220f40707e8abd90b098375200545ab12
Adam Mudrak <mudrets2000@gmail.com>
2025-01-16 23:34:53 +0200

    Add DTO to save page metadata

---
+++ src/main/java/com/example/budgetingapp/dtos/transactions/response/GetTransactionsPageDto.java
@@ -0,0 +1,10 @@
+package com.example.budgetingapp.dtos.transactions.response;
+
+import java.util.List;
+
+public record GetTransactionsPageDto(int pageNumber,
+                                     int pageSize,
+                                     int elementsPresentOnPage,
+                                     long totalElements,
+                                     int totalPages,
+                                     List<GetResponseTransactionDto> transactionsPageDtoList){}
