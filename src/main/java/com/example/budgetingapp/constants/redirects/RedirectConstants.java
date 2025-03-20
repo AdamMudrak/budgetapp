2f3cd3947ad9ff57459fb701d9b5c88878cff729
Adam Mudrak <mudrets2000@gmail.com>
2025-03-20 21:29:59 +0200

    Change domain

--- src/main/java/com/example/budgetingapp/constants/redirects/RedirectConstants.java
+++ src/main/java/com/example/budgetingapp/constants/redirects/RedirectConstants.java
@@ -1,8 +1,7 @@
 package com.example.budgetingapp.constants.redirects;
 
 public class RedirectConstants {
-    public static final String REGISTRATION_CONFIRMATION_LINK = "https://my.budgetapp.space/#/login";
-    public static final String PASSWORD_RESET_CONFIRMATION_LINK = "https://my.budgetapp.space/#/";
-    public static final String RANDOM_PASSWORD_REDIRECT_LINK = "https://my.budgetapp.space/#/login";
-
+    public static final String REGISTRATION_CONFIRMATION_LINK = "https://my.moneta-api.space/#/login";
+    public static final String PASSWORD_RESET_CONFIRMATION_LINK = "https://my.moneta-api.space/#/";
+    public static final String RANDOM_PASSWORD_REDIRECT_LINK = "https://my.moneta-api.space/#/login";
 }
