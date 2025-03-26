395e98f50f4f45cf70a60f60dfc089b330e610e8
Adam Mudrak <mudrets2000@gmail.com>
2025-03-26 16:56:11 +0200

    Rename volume constant

--- src/main/java/com/example/budgetingapp/constants/redirects/RedirectConstants.java
+++ src/main/java/com/example/budgetingapp/constants/redirects/RedirectConstants.java
@@ -4,8 +4,8 @@ public class RedirectConstants {
     public static final String REGISTRATION_CONFIRMATION_LINK = "${registration.confirmation.link}";
     public static final String PASSWORD_RESET_CONFIRMATION_LINK =
             "${password.reset.confirmation.link}";
-    public static final String PASSWORD_RESET_ERROR_LINK =
-            "${password.reset.error.link}";
+    public static final String ACTION_ERROR_LINK =
+            "${action.error.link}";
     public static final String RANDOM_PASSWORD_REDIRECT_LINK =
             "${get.random.password.redirect.link}";
 }
